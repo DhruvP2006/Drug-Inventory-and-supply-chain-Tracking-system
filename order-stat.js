76b9bf2deba1387306220852216246c736b39e7b
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-08-29 21:23:04 +0530

    Merged PO and Order stauts page to Purchase order

--- order-stat.js
+++ order-stat.js
@@ -34,3 +34,23 @@ function openPanel() {
 function closePanel() {
   document.getElementById("mySidePanel").style.width = "0";
 }
+
+// JavaScript to toggle the dropdown menu
+document.querySelector(".dropbtn").addEventListener("click", function () {
+  const dropdownContent = document.querySelector(".dropdown-content");
+  dropdownContent.style.display =
+    dropdownContent.style.display === "block" ? "none" : "block";
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+};
